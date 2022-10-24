2a5187d4380951d9b733f2baaa073f9e724ae796
Efrain <efrainbond@hotmail.com>
2022-10-24 14:02:21 -0500

    Agregar archivos de proyecto.

---
+++ TestDynamicTabPage/Data/CurrentPageService.cs
@@ -0,0 +1,49 @@
+﻿namespace TestDynamicTabPage.Data
+{
+    public class CurrentPageService
+    {
+        #region Construct
+        private int contador = 2;
+        public int Numero = 1;
+        public string NombrePaginaActual { get; set; }
+        public Type TypePaginaActual { get; set; }
+
+        public CurrentPageService()
+        {
+            NombrePaginaActual = "Home";
+            TypePaginaActual = Activator.CreateInstance("TestDynamicTabPage", $"TestDynamicTabPage.Pages.Index").Unwrap().GetType();
+        }
+        #endregion
+
+        public void CrearNuevaPagina(string nombreDelapagina, string nombreDelComponente)
+        {
+            NombrePaginaActual = nombreDelapagina;
+            Numero = contador;
+            contador++;
+            TypePaginaActual = Activator.CreateInstance("TestDynamicTabPage", $"{nombreDelComponente}").Unwrap().GetType();
+            NotifyStateChanged();
+        }
+
+        public Pagina GetPagina()
+        {
+            return new Pagina
+            {
+                Nombre = NombrePaginaActual,
+                Tipo = TypePaginaActual,
+                Numero = Numero
+            };
+        }
+
+        public void cambiarVentana(Pagina pagina)
+        {
+            this.NombrePaginaActual = pagina.Nombre;
+            this.Numero = pagina.Numero;
+            this.TypePaginaActual = pagina.Tipo;
+            NotifyStateChanged();
+        }
+        
+        public event Action OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();        
+    }
+}
---
+++ TestDynamicTabPage/Data/OpenedPagesService.cs
@@ -0,0 +1,55 @@
+﻿namespace TestDynamicTabPage.Data
+{
+    public class OpenedPagesService
+    {
+        #region Construct
+        public List<Pagina> Paginas { get; set; }
+        public Dictionary<int, Dictionary<string, object>> Estado { get; set; }
+
+        public OpenedPagesService()
+        {
+            this.Paginas = new List<Pagina>();
+            Paginas.Add(new Pagina
+            {
+                Numero = 1,
+                Nombre = "Home",
+                Tipo = Activator.CreateInstance("TestDynamicTabPage", $"TestDynamicTabPage.Pages.Index").Unwrap().GetType()
+            });
+            this.Estado = new Dictionary<int, Dictionary<string, object>>();
+            var Parametros = new Dictionary<string, object>();
+            this.Estado = new Dictionary<int, Dictionary<string, object>>();
+            this.Estado.Add(1, Parametros);
+        }
+        #endregion
+
+        public void agregarPaginaNueva(Pagina pagina)
+        {
+            this.Paginas.Add(pagina);
+            var Parametros = new Dictionary<string, object>();
+            this.Estado.Add(pagina.Numero, Parametros);
+        }
+
+        public void AgregarValorAlEstado(int numero, string key, object valor)
+        {
+            Estado[numero][key] = valor;
+            NotifyStateChanged();
+        }
+
+        public void cerrarPagina(Pagina pagina)
+        {
+            Paginas.Remove(pagina);
+            Estado.Remove(pagina.Numero);
+        }
+
+        public event Action OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+
+    public class Pagina
+    {
+        public string Nombre { get; set; }
+        public Type Tipo { get; set; }
+        public int Numero { get; set; }
+    }
+}
